0909e1b65ad968bc442b2ad337d65c22f8ab84aa
daniel_sanchez <danielsanchezaranja@gmail.com>
2023-03-11 13:48:33 +0900

    [feat] thread pool class creation

---
+++ include/thread_pool.hpp
@@ -0,0 +1,34 @@
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+/**
+ * Creates a thread pool with numThreads concurrent threads, any type of
+ * functions can be enqueued to the task queue. The class uses mutex and locks
+ * to guarantee thread safety. Iit is important to note that in does not handle
+ * thread safety for tasks with shared resources.
+ */
+class ThreadPool {
+ public:
+  explicit ThreadPool(int numThreads);
+  ~ThreadPool();
+
+  /*Enqueues a function along with its arguments to be called later in one of
+   * the pool's threads
+   * @param f: the function to call
+   * @param args: arguments for f
+   */
+  template <class F, class... Args>
+  void enqueue(F&& f, Args&&... args);
+
+ private:
+  std::vector<std::thread> threads;
+  std::queue<std::function<void()>> tasks;
+  std::mutex queueMutex;
+  std::condition_variable condition;
+  bool stop;
+};
---
+++ src/thread_pool.cpp
@@ -0,0 +1,40 @@
+#include "thread_pool.hpp"
+
+ThreadPool::ThreadPool(int numThreads) : stop(false) {
+  for (int i = 0; i < numThreads; ++i) {
+    threads.emplace_back([this] {
+      for (;;) {
+        std::function<void()> task;
+
+        {
+          std::unique_lock<std::mutex> lock(this->queueMutex);
+          this->condition.wait(
+              lock, [this] { return this->stop || !this->tasks.empty(); });
+          if (this->stop && this->tasks.empty()) return;
+          task = std::move(this->tasks.front());
+          this->tasks.pop();
+        }
+
+        task();
+      }
+    });
+  }
+}
+
+ThreadPool::~ThreadPool() {
+  {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    stop = true;
+  }
+  condition.notify_all();
+  for (std::thread& thread : threads) thread.join();
+}
+
+template <class F, class... Args>
+void ThreadPool::enqueue(F&& f, Args&&... args) {
+  {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    tasks.emplace([=] { std::invoke(f, args...); });
+  }
+  condition.notify_one();
+}
