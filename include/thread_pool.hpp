3172a9937cd1c4472fd70b0b7e5d188ff4e45c05
daniel_sanchez <danielsanchezaranja@gmail.com>
2023-03-11 15:54:50 +0900

    [documenting] adding definitions

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -17,7 +17,6 @@ class ThreadPool {
  public:
   explicit ThreadPool(int numThreads);
   ~ThreadPool();
-
   /*Enqueues a function along with its arguments to be called later in one of
    * the pool's threads
    * @param f: the function to call
@@ -27,11 +26,20 @@ class ThreadPool {
   inline void enqueue(F&& f, Args&&... args) {
     {
       std::unique_lock<std::mutex> lock(queueMutex);
+       // don't allow enqueueing after stopping the pool
+      if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
       tasks.emplace([=] { std::invoke(f, args...); });
     }
     condition.notify_one();
   };
 
+  /*Enqueues a function along with its arguments to be called later in one of
+   * the pool's threads. Returns a std::future that can be used to obtain the
+   * result of the function
+   * @param f: the function to call
+   * @param args: arguments for f
+   * @return result of function
+   */
   template <class F, class... Args>
   inline auto enqueue_result(F&& f, Args&&... args)
       -> std::future<typename std::result_of<F(Args...)>::type> {
